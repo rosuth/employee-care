f3f98f36093252f4146cc2e72f415e96ccd264fe
Rohit Suthar <rohitkumar1121.rk@gmail.com>
2020-12-04 19:42:09 +0530

    Configs Updated

--- sql/db.sql
+++ sql/db.sql
@@ -1,27 +1,12 @@
--- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: EMPLOYEECARE
--- ------------------------------------------------------
--- Server version	8.0.21-0ubuntu0.20.04.4
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP DATABASE IF EXISTS `EMPLOYEECARE`;
+CREATE DATABASE EMPLOYEECARE;
+USE EMPLOYEECARE;
 
 --
 -- Table structure for table `ADMIN`
 --
 
 DROP TABLE IF EXISTS `ADMIN`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ADMIN` (
   `AID` bigint NOT NULL AUTO_INCREMENT,
   `FIRSTNAME` varchar(255) NOT NULL,
@@ -30,16 +15,9 @@ CREATE TABLE `ADMIN` (
   `PASSWORD` varchar(255) NOT NULL,
   PRIMARY KEY (`AID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ADMIN`
---
 
 LOCK TABLES `ADMIN` WRITE;
-/*!40000 ALTER TABLE `ADMIN` DISABLE KEYS */;
-INSERT INTO `ADMIN` VALUES (1,'Rohit','Suthar','rohit@gmail.com','1234'),(2,'Rohit','Suthar','rohitf@gmail.com','1234');
-/*!40000 ALTER TABLE `ADMIN` ENABLE KEYS */;
+INSERT INTO `ADMIN` VALUES (1,'Robert','Almindo','robert@abc.com','JFH123D'),(2,'Lisa','Morgan','lisam@abc.com','8ADSH32');
 UNLOCK TABLES;
 
 --
@@ -47,8 +25,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `EMPLOYEE`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `EMPLOYEE` (
   `EID` bigint NOT NULL AUTO_INCREMENT,
   `FIRSTNAME` varchar(255) NOT NULL,
@@ -61,16 +37,11 @@ CREATE TABLE `EMPLOYEE` (
   `CITY` varchar(255) NOT NULL,
   PRIMARY KEY (`EID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `EMPLOYEE`
---
 
 LOCK TABLES `EMPLOYEE` WRITE;
-/*!40000 ALTER TABLE `EMPLOYEE` DISABLE KEYS */;
-INSERT INTO `EMPLOYEE` VALUES (1,'KL','Rahul','kl@rahul.com','10/10/1992','MALE','1234','Bangalore','Bangalore'),(2,'MS','Dhoni','ms@dhoni.com','10/10/1994','MALE','1234','Chennai','Chennai');
-/*!40000 ALTER TABLE `EMPLOYEE` ENABLE KEYS */;
+INSERT INTO `EMPLOYEE` VALUES 
+(1,'Peter','Beton','peterb@abc.com','10/10/1992','MALE','BJHFD12','Mexios','Uruntro'),
+(2,'Maria','Hord','maria@abc.com','04/04/1994','FEMALE','12JKHDF1','Juman','Kovenin');
 UNLOCK TABLES;
 
 --
@@ -78,8 +49,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `EMPLOYEELEAVE`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `EMPLOYEELEAVE` (
   `LID` bigint NOT NULL AUTO_INCREMENT,
   `EID` varchar(255) DEFAULT NULL,
@@ -92,16 +61,15 @@ CREATE TABLE `EMPLOYEELEAVE` (
   `REASON` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`LID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `EMPLOYEELEAVE`
 --
 
 LOCK TABLES `EMPLOYEELEAVE` WRITE;
-/*!40000 ALTER TABLE `EMPLOYEELEAVE` DISABLE KEYS */;
-INSERT INTO `EMPLOYEELEAVE` VALUES (1,'1','KL Rahul','kl@rahul.com','Casual','01/01/2000','01/02/2000','30','N/A'),(3,'2','MS Dhoni','ms@dhoni.com','Casual','01/01/2000','01/02/2000','30','N/A');
-/*!40000 ALTER TABLE `EMPLOYEELEAVE` ENABLE KEYS */;
+INSERT INTO `EMPLOYEELEAVE` VALUES 
+(1,'1','Peter Beton','peterb@abc.com','Casual','01/01/2020','01/02/2020','30','N/A'),
+(2,'2','Maria Hord','maria@abc.com','Holiday','10/09/2020','12/09/2020','2','N/A');
 UNLOCK TABLES;
 
 --
@@ -109,8 +77,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `ROLES`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ROLES` (
   `ID` bigint NOT NULL AUTO_INCREMENT,
   `ROLE_NAME` varchar(255) DEFAULT NULL,
@@ -125,25 +91,16 @@ CREATE TABLE `ROLES` (
   CONSTRAINT `FKhcc2i2yb19r3dq4yejfffvms8` FOREIGN KEY (`ROLE_ID`) REFERENCES `ROLES` (`ID`),
   CONSTRAINT `FKl44qvefvdtwr03dk6u1rv94jm` FOREIGN KEY (`ADMIN_ID`) REFERENCES `ADMIN` (`AID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `ROLES`
---
+SET FOREIGN_KEY_CHECKS=0;
 
 LOCK TABLES `ROLES` WRITE;
-/*!40000 ALTER TABLE `ROLES` DISABLE KEYS */;
-INSERT INTO `ROLES` VALUES (1,'ADMIN_ROLE',NULL,1,1),(2,'EMP_ROLE',1,2,NULL),(3,'EMP_ROLE',2,2,NULL);
-/*!40000 ALTER TABLE `ROLES` ENABLE KEYS */;
+INSERT INTO `ROLES` VALUES 
+(1,'ADMIN_ROLE',NULL,1,1),
+(2,'ADMIN_ROLE',NULL,2,1),
+(3,'EMP_ROLE',1,3,NULL),
+(4,'EMP_ROLE',2,4,NULL);
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SET FOREIGN_KEY_CHECKS=1;
 
--- Dump completed on 2020-12-02 16:09:22
